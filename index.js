e6b009414ac15cb66db091dfcd3e293d57fdf813
so1ve <nn_201312@163.com>
2022-09-02 21:19:06 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "./order.js",
+  ],
+  overrides: [
+    {
+      files: [
+        "*.vue",
+        "**/*.vue",
+        "*.html",
+        "**/*.html",
+      ],
+      customSyntax: require("postcss-html"),
+    },
+  ],
+};
